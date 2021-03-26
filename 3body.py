2b10dce8c058a6632df3649aa4c79da5f79c4c8c
Marie <mariehav@uio.no>
2021-03-26 10:32:20 +0100

    Lagt til basic algoritme

--- 3body.py
+++ 3body.py
@@ -1,2 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import constants as sc
+
+#Dicitonary:
+# h = sc.physical_constants["Planck constant in eV/Hz"][0] #eV/Hz
+
+
+def three_body_problem():
+
+    #N iterasjoner, 3 legemer, 2 dimensjoner (x,y)
+    r = np.zeros((N, 3, 2), float)
+    v = np.zeros((N, 3, 2), float)
+    a = np.zeros((N, 3, 2), float)
+
+    r[0,0,:] =
+    r[0,1,:] =
+    r[0,2,:] =
+
+    v[0,0,:] =
+    v[0,1,:] =
+    v[0,2,:] =
+
+    a[0,0,:] =
+    a[0,1,:] =
+    a[0,2,:] =
+
+    m0 =
+    m1 =
+    m2 =
+
+    dt = 0.1
+
+    for i in range(N):
+        a[i,0,:] = -sc.G(m1*(r[i,0,:] - r[i,1,:])/np.linalg.norm(r[i,0,:] - r[i,1,:])**3 + m2*(r[i,0,:] - r[i,2,:])/np.linalg.norm(r[i,0,:] - r[i,2,:])**3)
+        a[i,1,:] = -sc.G(m2*(r[i,1,:] - r[i,2,:])/np.linalg.norm(r[i,1,:] - r[i,2,:])**3 + m0*(r[i,1,:] - r[i,0,:])/np.linalg.norm(r[i,1,:] - r[i,0,:])**3)
+        a[i,2,:] = -sc.G(m0*(r[i,2,:] - r[i,0,:])/np.linalg.norm(r[i,2,:] - r[i,0,:])**3 + m1*(r[i,2,:] - r[i,1,:])/np.linalg.norm(r[i,2,:] - r[i,1,:])**3)
+        r[i+1,:,:] = r[i,:,:] + v[i,:,:]*dt + 0.5*a[i,:,:]*dt**2
+        a[i+1,0,:] = -sc.G(m1*(r[i+1,0,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,1,:])**3 + m2*(r[i+1,0,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,2,:])**3)
+        a[i+1,1,:] = -sc.G(m2*(r[i+1,1,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,2,:])**3 + m0*(r[i+1,1,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,0,:])**3)
+        a[i+1,2,:] = -sc.G(m0*(r[i+1,2,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,0,:])**3 + m1*(r[i+1,2,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,1,:])**3)
+        v[i+1,:,:] = v[i,:,:] + 0.5*(a[i,:,:] + a[i+1,:,:])*dt
