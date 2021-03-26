4bee8d44fa3c90d32643b5ad4129f99feb03f7f4
Marie <mariehav@uio.no>
2021-03-26 12:13:24 +0100

    Alt fungerer, får et greit plott

--- 3body.py
+++ 3body.py
@@ -6,37 +6,52 @@ from scipy import constants as sc
 # h = sc.physical_constants["Planck constant in eV/Hz"][0] #eV/Hz
 
 
-def three_body_problem():
+def three_body_problem(N):
+    solar_mass = 1.9891e+30
+    #G = 39.4784 #Gravitational constant in solar system units [AU^3/yr^2/m_sun]
+    G = 1
+
+    #Masse gitt i solmasse
+    m0 = 1.1
+    m1 = 0.907
+    m2 = 0.123
+    dt = 0.000001 #Tidssteg
 
     #N iterasjoner, 3 legemer, 2 dimensjoner (x,y)
     r = np.zeros((N, 3, 2), float)
     v = np.zeros((N, 3, 2), float)
     a = np.zeros((N, 3, 2), float)
 
-    r[0,0,:] =
-    r[0,1,:] =
-    r[0,2,:] =
+    r[0,0,:] = np.array([0.02,0])
+    r[0,1,:] = np.array([-0.02,0])
+    r[0,2,:] = np.array([0,0.02])
 
-    v[0,0,:] =
-    v[0,1,:] =
-    v[0,2,:] =
+    v[0,0,:] = np.array([2.3,3])
+    v[0,1,:] = np.array([5,1.4])
+    v[0,2,:] = np.array([3,3.5])
 
-    a[0,0,:] =
-    a[0,1,:] =
-    a[0,2,:] =
+    a[0,0,:] = -G*(m1*(r[0,0,:] - r[0,1,:])/np.linalg.norm(r[0,0,:] - r[0,1,:])**3 + m2*(r[0,0,:] - r[0,2,:])/np.linalg.norm(r[0,0,:] - r[0,2,:])**3)
+    a[0,1,:] = -G*(m2*(r[0,1,:] - r[0,2,:])/np.linalg.norm(r[0,1,:] - r[0,2,:])**3 + m0*(r[0,1,:] - r[0,0,:])/np.linalg.norm(r[0,1,:] - r[0,0,:])**3)
+    a[0,2,:] = -G*(m0*(r[0,2,:] - r[0,0,:])/np.linalg.norm(r[0,2,:] - r[0,0,:])**3 + m1*(r[0,2,:] - r[0,1,:])/np.linalg.norm(r[0,2,:] - r[0,1,:])**3)
 
-    m0 =
-    m1 =
-    m2 =
-
-    dt = 0.1
-
-    for i in range(N):
-        a[i,0,:] = -sc.G(m1*(r[i,0,:] - r[i,1,:])/np.linalg.norm(r[i,0,:] - r[i,1,:])**3 + m2*(r[i,0,:] - r[i,2,:])/np.linalg.norm(r[i,0,:] - r[i,2,:])**3)
-        a[i,1,:] = -sc.G(m2*(r[i,1,:] - r[i,2,:])/np.linalg.norm(r[i,1,:] - r[i,2,:])**3 + m0*(r[i,1,:] - r[i,0,:])/np.linalg.norm(r[i,1,:] - r[i,0,:])**3)
-        a[i,2,:] = -sc.G(m0*(r[i,2,:] - r[i,0,:])/np.linalg.norm(r[i,2,:] - r[i,0,:])**3 + m1*(r[i,2,:] - r[i,1,:])/np.linalg.norm(r[i,2,:] - r[i,1,:])**3)
+    #Leap-frog
+    for i in range(N-1):
+        a[i,0,:] = -G*(m1*(r[i,0,:] - r[i,1,:])/np.linalg.norm(r[i,0,:] - r[i,1,:])**3 + m2*(r[i,0,:] - r[i,2,:])/np.linalg.norm(r[i,0,:] - r[i,2,:])**3)
+        a[i,1,:] = -G*(m2*(r[i,1,:] - r[i,2,:])/np.linalg.norm(r[i,1,:] - r[i,2,:])**3 + m0*(r[i,1,:] - r[i,0,:])/np.linalg.norm(r[i,1,:] - r[i,0,:])**3)
+        a[i,2,:] = -G*(m0*(r[i,2,:] - r[i,0,:])/np.linalg.norm(r[i,2,:] - r[i,0,:])**3 + m1*(r[i,2,:] - r[i,1,:])/np.linalg.norm(r[i,2,:] - r[i,1,:])**3)
         r[i+1,:,:] = r[i,:,:] + v[i,:,:]*dt + 0.5*a[i,:,:]*dt**2
-        a[i+1,0,:] = -sc.G(m1*(r[i+1,0,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,1,:])**3 + m2*(r[i+1,0,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,2,:])**3)
-        a[i+1,1,:] = -sc.G(m2*(r[i+1,1,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,2,:])**3 + m0*(r[i+1,1,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,0,:])**3)
-        a[i+1,2,:] = -sc.G(m0*(r[i+1,2,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,0,:])**3 + m1*(r[i+1,2,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,1,:])**3)
+        a[i+1,0,:] = -G*(m1*(r[i+1,0,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,1,:])**3 + m2*(r[i+1,0,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,2,:])**3)
+        a[i+1,1,:] = -G*(m2*(r[i+1,1,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,2,:])**3 + m0*(r[i+1,1,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,0,:])**3)
+        a[i+1,2,:] = -G*(m0*(r[i+1,2,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,0,:])**3 + m1*(r[i+1,2,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,1,:])**3)
         v[i+1,:,:] = v[i,:,:] + 0.5*(a[i,:,:] + a[i+1,:,:])*dt
+
+    planet_names = ['Alpha Centauri A', 'Alpha Centauri B', 'Proxima Centauri']
+    for i in range(3):
+            plt.plot(r[:,i,0],r[:,i,1], label=f"{planet_names[i]}")
+
+    #plt.plot([-1,0,0],r[-1,0,1], style="dotted")
+    plt.legend()
+    plt.show()
+
+
+three_body_problem(10000)
