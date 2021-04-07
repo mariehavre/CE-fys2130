87920c0b12c2a28a5f9edd3e776df16ebcd02cd4
Marie <mariehav@uio.no>
2021-04-07 13:13:05 +0200

    testtest

--- 3body.py
+++ 3body.py
@@ -1,27 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import constants as sc
-
-#Dicitonary:
-# h = sc.physical_constants["Planck constant in eV/Hz"][0] #eV/Hz
-
 
 def three_body_problem(N):
     solar_mass = 1.9891e+30
-    #G = 39.4784 #Gravitational constant in solar system units [AU^3/yr^2/m_sun]
-    G = 1
+    G = 39.4784 #Gravitational constant in solar system units [AU^3/yr^2/m_sun]
+    #G = 1
 
     #Masse gitt i solmasse
     m0 = 1.1
     m1 = 0.907
     m2 = 0.123
-    dt = 0.000001 #Tidssteg
+    #dt = 0.000001 #Tidssteg
+    dt = 0.0000001 #Tidssteg
 
     #N iterasjoner, 3 legemer, 2 dimensjoner (x,y)
     r = np.zeros((N, 3, 2), float)
     v = np.zeros((N, 3, 2), float)
     a = np.zeros((N, 3, 2), float)
 
+    #Disse fungerer
     r[0,0,:] = np.array([0.02,0])
     r[0,1,:] = np.array([-0.02,0])
     r[0,2,:] = np.array([0,0.02])
@@ -29,6 +26,21 @@ def three_body_problem(N):
     v[0,0,:] = np.array([2.3,3])
     v[0,1,:] = np.array([5,1.4])
     v[0,2,:] = np.array([3,3.5])
+
+    #Tester initialhastighet på 0
+    # v[0,0,:] = np.array([0,0])
+    # v[0,1,:] = np.array([0,0])
+    # v[0,2,:] = np.array([0,0])
+
+    #Tester betingelser
+    # r[0,0,:] = np.array([0.04,0])
+    # r[0,1,:] = np.array([0,0])
+    # r[0,2,:] = np.array([0,0.02])
+    #
+    # v[0,0,:] = np.array([2.3,3])
+    # v[0,1,:] = np.array([1.5,3])
+    # v[0,2,:] = np.array([3,3.5])
+    #Slutt på testbetingelser
 
     a[0,0,:] = -G*(m1*(r[0,0,:] - r[0,1,:])/np.linalg.norm(r[0,0,:] - r[0,1,:])**3 + m2*(r[0,0,:] - r[0,2,:])/np.linalg.norm(r[0,0,:] - r[0,2,:])**3)
     a[0,1,:] = -G*(m2*(r[0,1,:] - r[0,2,:])/np.linalg.norm(r[0,1,:] - r[0,2,:])**3 + m0*(r[0,1,:] - r[0,0,:])/np.linalg.norm(r[0,1,:] - r[0,0,:])**3)
@@ -45,13 +57,38 @@ def three_body_problem(N):
         a[i+1,2,:] = -G*(m0*(r[i+1,2,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,0,:])**3 + m1*(r[i+1,2,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,1,:])**3)
         v[i+1,:,:] = v[i,:,:] + 0.5*(a[i,:,:] + a[i+1,:,:])*dt
 
+    '''
     planet_names = ['Alpha Centauri A', 'Alpha Centauri B', 'Proxima Centauri']
     for i in range(3):
             plt.plot(r[:,i,0],r[:,i,1], label=f"{planet_names[i]}")
+            plt.plot(r[0,i,0],r[0,i,1], "ro", markersize=3) #Plotter startposisjon
+            plt.plot(r[-1,i,0],r[-1,i,1], "ko", markersize=3) #Plotter sluttposisjon
 
-    #plt.plot([-1,0,0],r[-1,0,1], style="dotted")
     plt.legend()
     plt.show()
+    '''
+
+    '''
+    #Tester Fourieranalyse
+    fig, ax = plt.subplots(2,1, figsize=(13,8))
+    #X_k = (1/N)*np.fft.fft2(r[:,0,:]) #Todimensjonal array
+    X_k = (1/N)*np.fft.fft(r[:,0,0]) #Endimensjonal array
+    freq = np.fft.fftfreq(N, dt)
+    ax[1].plot(freq, np.abs(X_k[:N]), color='black', linewidth=0.2)
+    ax[1].set_xlabel('f [Hz]')
+    ax[1].set_ylabel('X$_k$')
+    ax[1].set_xlim([0, 1000])
+    plt.show()
+
+    plt.imshow(np.log(np.abs(np.fft.fftshift(X_k))**2))
+    plt.show()
+    '''
+
+    #Kopiert fra eksempel:
+    # X_k = (1/N)*np.fft.fft(x_n)
+    # freq = np.fft.fftfreq(N, dt)
 
 
-three_body_problem(10000)
+
+three_body_problem(500000)
+#three_body_problem(5000)
