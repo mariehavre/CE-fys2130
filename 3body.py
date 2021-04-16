a27edb928fd92311a8f48117f47364a49a2ea04e
Marie <mariehav@uio.no>
2021-04-16 16:29:52 +0200

    Ryddet enda mer, fjernet leapfrog

--- 3body.py
+++ 3body.py
@@ -11,6 +11,7 @@ def three_body_problem(N):
     m2 = 0.123
 
     dt = 0.0000001 #Tidssteg
+    #dt = 0.0000005 #Morsomt tidssteg
     T_end = N*dt #Sluttid
     t = np.linspace(0, T_end, N)
 
@@ -35,19 +36,8 @@ def three_body_problem(N):
     a[0,1,:] = -G*(m2*(r[0,1,:] - r[0,2,:])/np.linalg.norm(r[0,1,:] - r[0,2,:])**3 + m0*(r[0,1,:] - r[0,0,:])/np.linalg.norm(r[0,1,:] - r[0,0,:])**3)
     a[0,2,:] = -G*(m0*(r[0,2,:] - r[0,0,:])/np.linalg.norm(r[0,2,:] - r[0,0,:])**3 + m1*(r[0,2,:] - r[0,1,:])/np.linalg.norm(r[0,2,:] - r[0,1,:])**3)
 
-    #Integrerer med leap-frog-metoden
+    #Integrerer med Euler-Cromer
     for i in range(N-1):
-        # a[i,0,:] = -G*(m1*(r[i,0,:] - r[i,1,:])/np.linalg.norm(r[i,0,:] - r[i,1,:])**3 + m2*(r[i,0,:] - r[i,2,:])/np.linalg.norm(r[i,0,:] - r[i,2,:])**3)
-        # a[i,1,:] = -G*(m2*(r[i,1,:] - r[i,2,:])/np.linalg.norm(r[i,1,:] - r[i,2,:])**3 + m0*(r[i,1,:] - r[i,0,:])/np.linalg.norm(r[i,1,:] - r[i,0,:])**3)
-        # a[i,2,:] = -G*(m0*(r[i,2,:] - r[i,0,:])/np.linalg.norm(r[i,2,:] - r[i,0,:])**3 + m1*(r[i,2,:] - r[i,1,:])/np.linalg.norm(r[i,2,:] - r[i,1,:])**3)
-        # r[i+1,:,:] = r[i,:,:] + v[i,:,:]*dt + 0.5*a[i,:,:]*dt**2
-        # a[i+1,0,:] = -G*(m1*(r[i+1,0,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,1,:])**3 + m2*(r[i+1,0,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,0,:] - r[i+1,2,:])**3)
-        # a[i+1,1,:] = -G*(m2*(r[i+1,1,:] - r[i+1,2,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,2,:])**3 + m0*(r[i+1,1,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,1,:] - r[i+1,0,:])**3)
-        # a[i+1,2,:] = -G*(m0*(r[i+1,2,:] - r[i+1,0,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,0,:])**3 + m1*(r[i+1,2,:] - r[i+1,1,:])/np.linalg.norm(r[i+1,2,:] - r[i+1,1,:])**3)
-        # v[i+1,:,:] = v[i,:,:] + 0.5*(a[i,:,:] + a[i+1,:,:])*dt
-        # t[i+1] = t[i] + dt
-
-        #Euler-Cromer
         a[i+1,0,:] = -G*(m1*(r[i,0,:] - r[i,1,:])/np.linalg.norm(r[i,0,:] - r[i,1,:])**3 + m2*(r[i,0,:] - r[i,2,:])/np.linalg.norm(r[i,0,:] - r[i,2,:])**3)
         a[i+1,1,:] = -G*(m2*(r[i,1,:] - r[i,2,:])/np.linalg.norm(r[i,1,:] - r[i,2,:])**3 + m0*(r[i,1,:] - r[i,0,:])/np.linalg.norm(r[i,1,:] - r[i,0,:])**3)
         a[i+1,2,:] = -G*(m0*(r[i,2,:] - r[i,0,:])/np.linalg.norm(r[i,2,:] - r[i,0,:])**3 + m1*(r[i,2,:] - r[i,1,:])/np.linalg.norm(r[i,2,:] - r[i,1,:])**3)
@@ -57,15 +47,14 @@ def three_body_problem(N):
 
     return r, v, t
 
-def plot_position(N):
-    r, v, t = three_body_problem(N)
-
-    star_names = ['Alpha Centauri A', 'Alpha Centauri B', 'Proxima Centauri']
+def plot_position(r, N):
     for i in range(3):
             plt.plot(r[:,i,0],r[:,i,1], label=f"{star_names[i]}")
             plt.plot(r[0,i,0],r[0,i,1], "ro", markersize=3) #Plotter startposisjon
             plt.plot(r[-1,i,0],r[-1,i,1], "ko", markersize=3) #Plotter sluttposisjon
-
+    plt.title("Posisjonene til stjernene i stjernesystemet")
+    plt.xlabel("$r_x$ [AU]")
+    plt.ylabel("$r_y$ [AU]")
     plt.legend()
     plt.show()
 
@@ -77,16 +66,21 @@ def doppler_shift(v, f_s):
     f = f_s*np.sqrt((1-v/c)/(1+v/c))
     return f
 
-def plot_doppler(f_s, N, planet_nr):
-    r, v, t = three_body_problem(N)
-    f_dopp = doppler_shift(v[:,planet_nr,0], f_s)
-    plt.plot(t, f_dopp)
+def plot_doppler(v, f_s, N):
+    for i in range(3):
+        f_dopp = doppler_shift(v[:,i,0], f_s)
+        plt.plot(t, f_dopp, label=f"{star_names[i]}")
+    plt.title("Dopplerforskyvningen av signalet til hver stjerne")
+    plt.xlabel("Tid [År]")
+    plt.ylabel("Frekvens [MHz]")
+    plt.legend()
+    plt.show()
 
 def f_to_lambda(v, f_s):
     freq = doppler_shift(v, f_s)
     f = freq*1e+6 #Går fra MHz til Hz
-    c = 3e+8
-    lmbda = c/f
+    c = 3e+8 #Lysets hastighet [m/s]
+    lmbda = c/f #Bølgelengde [m]
     return lmbda
 
 def refractive_index(v, f_s):
@@ -98,38 +92,26 @@ def refractive_index(v, f_s):
     return n
 
 def focal_length(v, f_s):
-    R1 = 0.5
-    R2 = 0.7
+    R1 = -0.5
+    R2 = -0.7
     n = refractive_index(v, f_s)
-    return (n-1)*(1/R1 - 1/R2) #1/f
+    return 1/(n-1)*(1/R1 - 1/R2) #f
+
+def plot_focal_length(v, f_s):
+    for i in range(3):
+        plt.plot(t, focal_length(v[:,i,0], f_s), label=f"{star_names[i]}")
+    plt.title("Endring i brennvidde for hver stjerne")
+    plt.xlabel("Tid [år]")
+    plt.ylabel("Brennvidden til teleskopet [m]")
+    plt.legend()
+    plt.show()
+
 
 N = 69000 #tidssteg
 f_s = 980 #980 MHz
+r, v, t = three_body_problem(N) #Henter ut posisjon, hastighet og tidsarray
+star_names = ['Alfa Centauri A', 'Alfa Centauri B', 'Proxima Centauri']
 
-#Lagrer arrayene
-# r, v, t = three_body_problem(N) #KAN BRUKE DENNE ISTEDENFOR Å LOADE TING VED LEVERING
-# np.save('posisjon.npy', r)
-# np.save('hastighet.npy', v)
-# np.save('tid.npy', t)
-
-# v = np.load('hastighet.npy')
-# t = np.load('tid.npy')
-
-plot_position(N)
-# plot_doppler(f_s, N, 0)
-# plot_doppler(f_s, N, 1)
-# plot_doppler(f_s, N, 2)
-# plt.show()
-
-# plt.plot(t, focal_length(v[:,0,0], f_s))
-# plt.xlabel("Tid [år]")
-# plt.ylabel("Brennvidde (1/f)")
-#
-# plt.plot(t, focal_length(v[:,1,0], f_s))
-# plt.xlabel("Tid [år]")
-# plt.ylabel("Brennvidde (1/f)")
-#
-# plt.plot(t, focal_length(v[:,2,0], f_s))
-# plt.xlabel("Tid [år]")
-# plt.ylabel("Brennvidde (1/f)")
-# plt.show()
+plot_position(r, N)
+plot_doppler(v, f_s, N)
+plot_focal_length(v, f_s)
